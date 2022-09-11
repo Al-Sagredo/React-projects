d7e3f325e521e00faf3a52c17b9ffe7d581b5f14
TheIceCreaMan <alvergarax@gmail.com>
2022-09-11 08:52:57 -0400

    galeria imagenes con react y bootstrap

---
+++ galeria-imagenes-react/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return(
+        <div>
+            <h5>Pagina creada por Alvaro Sagredo</h5>
+        </div>
+    )
+}
+
+export default Footer
---
+++ galeria-imagenes-react/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = ({ title }) => {
+    return (
+        <div className="p-5 bg-secondary">
+            <h1 id="title">{title}</h1>
+        </div>
+
+    )
+}
+
+export default Header;
